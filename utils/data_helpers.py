7c91ac30eb4fb810e5c72cf7470d0b3e00e81f04
fbinbin <1057688937@qq.com>
2022-05-26 16:51:10 +0800

    增加数据集

---
+++ utils/data_helpers.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('../')
+
+import transformers
+from model.config import Config
+
+class Vocab:
+    UNK = '[UNK]'
+    def __init__(self, vocab_path):
+        pass
+    def __getitem__(self, token):
+        pass
+    def __len__(self):
+        pass
+
+def build_vocab(vocab_path):
+    pass
+
+if __name__ == '__main__':
+    pass
